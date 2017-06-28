e0a3cb289d3239d5a7898d2e46d7e65be34d34ab
gladjos <gladijos@gmail.com>
2017-06-29 00:03:46 +0300

    generic simple app

---
+++ QueueTestApp/ClassQueue/Queue.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassQueue
+{
+    public class Queue<T>
+    {
+        protected T[] storage;
+        protected const int defaultSize = 16;
+        protected int allocedSize;
+        private int head;
+        private int tail;
+        public int Size() { return tail - head; }
+        public void PrintSize()
+        {
+            Console.WriteLine("own queue size = {0}, capacity = {1}", Size(), allocedSize);
+        }
+        public Queue(int startSize = defaultSize)
+        {
+            allocedSize = startSize;
+            storage = new T[allocedSize];
+            head = 0;
+            tail = 0;
+            Console.WriteLine("new own queue created with capacity = {0}", allocedSize);
+        }
+        public void Push(T elem)
+        {
+            if (Size() == allocedSize)
+            {
+                allocedSize = allocedSize * 2;
+                T[] new_storage = new T[allocedSize];
+                storage.CopyTo(new_storage, head);
+                head = 0;
+                tail = Size();
+                storage = new_storage;
+                Console.WriteLine("queue realloced. current size = {0}", Size());
+            }
+            storage[tail++] = elem;
+            Console.WriteLine("new elem pushed to queue. current size = {0}", Size());
+
+        }
+        public T Pop()
+        {
+            if (head < tail)
+            {
+                Console.WriteLine("head elem from queue. current size = {0}", Size() -1);
+                return storage[head++];
+            }
+            Console.WriteLine("Just return default value of elem type. current size = {0}", Size());
+            return default(T);
+        }
+    }
+}
---
+++ QueueTestApp/QueueTestApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassQueue;
+
+namespace QueueTestApp
+{
+    class MyOwnSuperClass
+    {
+        public MyOwnSuperClass(int newIntegerVar = 0, string newStringVar = "")
+        {
+            integerVar = newIntegerVar;
+            stringVar = newStringVar;
+        }
+        public int integerVar;
+        public string stringVar;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassQueue.Queue<MyOwnSuperClass> myQueue = new ClassQueue.Queue<MyOwnSuperClass>();
+            myQueue.Push(new MyOwnSuperClass(1, "one"));
+            myQueue.Pop();
+            myQueue.Push(new MyOwnSuperClass(2, "two"));
+            myQueue.Push(new MyOwnSuperClass(4, "four"));
+            myQueue.Push(new MyOwnSuperClass(3, "three"));
+            myQueue.Pop();
+            myQueue.Pop();
+            myQueue.Pop();
+            myQueue.Pop();
+            
+        }
+    }
+}
